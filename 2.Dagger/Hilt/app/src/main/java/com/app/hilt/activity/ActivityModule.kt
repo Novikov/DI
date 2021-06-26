75dd60659b30df684d104cb519e3938a11e6af4e
Igor Novikov <novikov.mail@inbox.ru>
2021-06-26 19:17:46 +0300

    context providing works

--- 2.Dagger/Hilt/app/src/main/java/com/app/hilt/activity/ActivityModule.kt
+++ 2.Dagger/Hilt/app/src/main/java/com/app/hilt/activity/ActivityModule.kt
@@ -1,17 +1,20 @@
 package com.app.hilt.activity
 
+import android.content.Context
+import com.app.hilt.R
 import com.app.hilt.activity.ActivityString
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.qualifiers.ActivityContext
 
 @Module
 @InstallIn(ActivityComponent::class)
 class ActivityModule {
     @ActivityString
     @Provides
-    fun provideString():String{
-        return "String from activity module"
+    fun provideString(@ActivityContext context: Context):String{
+        return "String from activity module" + context.resources.getString(R.string.test_string)
     }
 }
